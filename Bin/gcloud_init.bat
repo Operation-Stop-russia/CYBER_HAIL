b73c21eabde589e3822532d9d724d1d687673ac1
Operation-Stop-russia <103530329+Operation-Stop-russia@users.noreply.github.com>
2022-05-16 16:07:41 +0300

    Add files via upload

--- Bin/gcloud_init.bat
+++ Bin/gcloud_init.bat
@@ -1,5 +1,8 @@
 @Echo off
-FOR /L %%G IN (1,1,600) DO (
+
+(echo 1 && echo y && echo n) | gcloud init --skip-diagnostics
+
+FOR /L %%G IN (2,1,600) DO (
 (echo 1 && echo %%G && echo n) | gcloud init --skip-diagnostics
 )
 Exit
