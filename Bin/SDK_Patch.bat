dd6cd7c6f27b4c16aa5c99140f3fe886fb7ee121
Operation-Stop-russia <103530329+Operation-Stop-russia@users.noreply.github.com>
2022-05-17 15:36:00 +0300

    Delete SDK_Patch.bat

--- Bin/SDK_Patch.bat
+++
@@ -1,3 +0,0 @@
-Copy /Y ssh.py "C:\Program Files (x86)\Google\Cloud SDK\google-cloud-sdk\lib\googlecloudsdk\command_lib\util\ssh"
-Copy /Y Command(Operation-Stop_russia).txt C:\Windows\Temp\Command(Operation-Stop_russia).txt
-exit
