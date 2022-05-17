f6a492c1ee38fc8d7360d5a759917824f271f53f
Operation-Stop-russia <103530329+Operation-Stop-russia@users.noreply.github.com>
2022-05-17 18:05:46 +0300

    Update Запускатор.bat

--- Sources (*.bat and *.exe in readable formats)/Запускатор.bat
+++ Sources (*.bat and *.exe in readable formats)/Запускатор.bat
@@ -6,48 +6,48 @@ Echo.
 Echo.
 :m1
 Echo.
-Echo 			"ŠI… €„"		 (Operation "Stop russia")
+Echo 			"КIБЕР ГРАД"		 (Operation "Stop russia")
 Echo.
-Echo 	“˘ Ł ! „«ď ăáŻič­®Ł® ŕ®§Ł®ŕâ ­­ď ¬ ó ˇăâ¨ ˘áâ ­®˘«Ą­¨© i Żŕ®Ż âçĄ­¨© GoogleCloudSDK
-Echo 		â  ˘¨Ş®­ ­  Żŕ®ćĄ¤ăŕ  i­ići «i§ ćiő  Ş ă­âi˘ (gcloud_init) !!!
+Echo 	Увага! Для успiшного розгортання має бути встановлений i пропатчений GoogleCloudSDK
+Echo 		та виконана процедура iнiцiалiзацiї акаунтiв (gcloud_init) !!!
 Echo.
-Echo 	’ŕ¨˘ «iáâě áĄ ­áă GoogleShell - 12 Ł®¤¨­. ’¨¦­Ą˘  Ş˘®â  ­  ˘¨Ş®ŕ¨áâ ­­ď GShell  Ş ă­â®¬ - 60 Ł®¤¨­.
-Echo 	„«ď § ˇĄ§ŻĄçĄ­­ď ŕĄ¦¨¬ă 24/7 Ż®âŕiˇ­® ¬ â¨ 3 Ş®¬Ż«ĄŞâ¨  Ş ă­âi˘ ­  1 ŹŠ i ˘¨Ş®ŕ¨áâ®˘ă˘ â¨ őĺ Ż®çĄŕ§i.
+Echo 	Тривалiсть сеансу GoogleShell - 12 годин. Тижнева квота на використання GShell акаунтом - 60 годин.
+Echo 	Для забезпечення режиму 24/7 потрiбно мати 3 комплекти акаунтiв на 1 ПК i використовувати їх почерзi.
 Echo.
-Echo 	„«ď ­ « čâă˘ ­­ď CloudSDK:
-Echo 	˘˘Ą¤iâě "0"   é®ˇ § ˘ ­â ¦¨â¨ â  ˘áâ ­®˘¨â¨ GoogleCloudSDK
-Echo 	˘˘Ą¤iâě "00"  é®ˇ § Żăáâ¨â¨ gcloud_init.bat (¤«ď ¤®¤ ˘ ­­ď  Ş ă­âi˘!)
-Echo 	˘˘Ą¤iâě "000" é®ˇ ˘¨Ş®­ â¨ Patch GoogleSDK (!)
+Echo 	Для налаштування CloudSDK:
+Echo 	введiть "0"   щоб завантажити та встановити GoogleCloudSDK
+Echo 	введiть "00"  щоб запустити gcloud_init.bat (для додавання акаунтiв!)
+Echo 	введiть "000" щоб виконати Patch GoogleSDK (!)
 Echo.
-Echo 	„«ď ŕ®§Ł®ŕâ ­­ď â  § ŻăáŞă  â Ş¨:
+Echo 	Для розгортання та запуску атаки:
 Echo.
-Echo 	˘˘Ą¤iâě "1" ¤«ď § ŻăáŞă 50 áĄ ­ái˘ ( Ş ă­â¨ 1-50)
-Echo 	˘˘Ą¤iâě "2" ¤«ď § ŻăáŞă 100 áĄ ­ái˘ ( Ş ă­â¨ 1-100)
-Echo 	˘˘Ą¤iâě "3" ¤«ď § ŻăáŞă 200 áĄ ­ái˘ ( Ş ă­â¨ 1-200)
+Echo 	введiть "1" для запуску 50 сеансiв (акаунти 1-50)
+Echo 	введiть "2" для запуску 100 сеансiв (акаунти 1-100)
+Echo 	введiть "3" для запуску 200 сеансiв (акаунти 1-200)
 Echo.
-Echo 	˘i¤Ż®˘i¤­®:
-Echo 	"11"  - 50 áĄ ­ái˘ ( Ş ă­â¨ 51-100 (¤ŕăŁ¨© Ş®¬Ż«ĄŞâ))
-Echo 	"111" - 50 áĄ ­ái˘ ( Ş ă­â¨ 101-150 (âŕĄâi© Ş®¬Ż«ĄŞâ))
-Echo 	"22"  - 100 áĄ ­ái˘ ( Ş ă­â¨ 101-200 (¤ŕăŁ¨© Ş®¬Ż«ĄŞâ))
-Echo 	"222" - 100 áĄ ­ái˘ ( Ş ă­â¨ 201-301 (âŕĄâi© Ş®¬Ż«ĄŞâ))
-Echo 	"33"  - 200 áĄ ­ái˘ ( Ş ă­â¨ 201-400 (¤ŕăŁ¨© Ş®¬Ż«ĄŞâ))
-Echo 	"333" - 200 áĄ ­ái˘ ( Ş ă­â¨ 401-600 (âŕĄâi© Ş®¬Ż«ĄŞâ))
+Echo 	вiдповiдно:
+Echo 	"11"  - 50 сеансiв (акаунти 51-100 (другий комплект))
+Echo 	"111" - 50 сеансiв (акаунти 101-150 (третiй комплект))
+Echo 	"22"  - 100 сеансiв (акаунти 101-200 (другий комплект))
+Echo 	"222" - 100 сеансiв (акаунти 201-301 (третiй комплект))
+Echo 	"33"  - 200 сеансiв (акаунти 201-400 (другий комплект))
+Echo 	"333" - 200 сеансiв (акаунти 401-600 (третiй комплект))
 Echo.
-Set /p choice=" ‚ č ˘¨ˇiŕ: "
+Set /p choice=" Ваш вибiр: "
 if not defined choice goto m1
 if "%choice%"=="0" (powershell -executionpolicy bypass -file Bin\InstallSDK.ps1) && echo Google Cloud SDK install running && goto m1
-if "%choice%"=="00" (Start "" "Bin\gcloud_init.bat") && echo ĄŞ®¬Ą­¤ăóâěáď ®¤ŕ §ă ¤®¤ â¨ ˘ái âŕ¨ Ş®¬Ż«ĄŞâ¨  Ş ă­âi˘ && goto m1
-if "%choice%"=="000" (Start "" "Bin\SDK_Patcher.exe") && echo Ź âçĄŕ § ŻăéĄ­® && goto m1
-if "%choice%"=="1" (Start "‡ ŻăáŞ â®ŕ" "Bin\Start50.bat") && exit
-if "%choice%"=="11" (Start "‡ ŻăáŞ â®ŕ" "Bin\Start50_2.bat") && exit
-if "%choice%"=="111" (Start "‡ ŻăáŞ â®ŕ" "Bin\Start50_3.bat") && exit
-if "%choice%"=="2" (Start "‡ ŻăáŞ â®ŕ" "Bin\Start100.bat") && exit
-if "%choice%"=="22" (Start "‡ ŻăáŞ â®ŕ" "Bin\Start100_2.bat") && exit
-if "%choice%"=="222" (Start "‡ ŻăáŞ â®ŕ" "Bin\Start100_3.bat") && exit
-if "%choice%"=="3" (Start "‡ ŻăáŞ â®ŕ" "Bin\Start200.bat") && exit
-if "%choice%"=="33" (Start "‡ ŻăáŞ â®ŕ" "Bin\Start200_2.bat") && exit
-if "%choice%"=="333" (Start "‡ ŻăáŞ â®ŕ" "Bin\Start100_3.bat") && exit
+if "%choice%"=="00" (Start "" "Bin\gcloud_init.bat") && echo Рекомендується одразу додати всi три комплекти акаунтiв && goto m1
+if "%choice%"=="000" (Start "" "Bin\SDK_Patcher.exe") && echo Патчер запущено && goto m1
+if "%choice%"=="1" (Start "Запускатор" "Bin\Start50.bat") && exit
+if "%choice%"=="11" (Start "Запускатор" "Bin\Start50_2.bat") && exit
+if "%choice%"=="111" (Start "Запускатор" "Bin\Start50_3.bat") && exit
+if "%choice%"=="2" (Start "Запускатор" "Bin\Start100.bat") && exit
+if "%choice%"=="22" (Start "Запускатор" "Bin\Start100_2.bat") && exit
+if "%choice%"=="222" (Start "Запускатор" "Bin\Start100_3.bat") && exit
+if "%choice%"=="3" (Start "Запускатор" "Bin\Start200.bat") && exit
+if "%choice%"=="33" (Start "Запускатор" "Bin\Start200_2.bat") && exit
+if "%choice%"=="333" (Start "Запускатор" "Bin\Start100_3.bat") && exit
 Echo.
 Echo.
-Echo  ! ŤĄ Ş®ŕĄŞâ­Ą ˘˘Ą¤Ą­­ď
+Echo  ! Не коректне введення
 goto m1
