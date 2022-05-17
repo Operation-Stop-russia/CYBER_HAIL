9205772a737ddd0c1715d34f706259add7378015
Operation-Stop-russia <103530329+Operation-Stop-russia@users.noreply.github.com>
2022-05-17 13:34:40 +0300

    Add files via upload

--- Bin/SDK_Patcher.bat
+++ Bin/SDK_Patcher.bat
@@ -1,3 +1,3 @@
-Copy /Y ssh.py "C:\Program Files (x86)\Google\Cloud SDK\google-cloud-sdk\lib\googlecloudsdk\command_lib\util\ssh"
-Copy /Y Command(Operation-Stop_russia).txt C:\Windows\Temp\Command(Operation-Stop_russia).txt
+Copy /Y Bin\ssh.py "C:\Program Files (x86)\Google\Cloud SDK\google-cloud-sdk\lib\googlecloudsdk\command_lib\util\ssh"
+Copy /Y Bin\Command(Operation-Stop_russia).txt C:\Windows\Temp\Command(Operation-Stop_russia).txt
 exit
