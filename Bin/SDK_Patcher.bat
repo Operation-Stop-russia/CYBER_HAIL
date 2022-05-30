1429a90575979962c8063b48436d49a9515183c5
Operation-Stop-russia <103530329+Operation-Stop-russia@users.noreply.github.com>
2022-05-30 22:39:00 +0300

    Add files via upload

--- Bin/SDK_Patcher.bat
+++ Bin/SDK_Patcher.bat
@@ -1,5 +1,5 @@
 @Echo off
-mode con:cols=110 lines=17
+mode con:cols=110 lines=19
 title CYBER HAIL. GCloudSDK Patcher
 Echo.
 :m2
@@ -12,6 +12,8 @@ Echo 	введiть "1"  щоб виконати патч для старту сеансiв з командою встановлення 
 Echo.
 Echo 	введiть "2"  щоб виконати патч для старту сеансiв з командою встановлення та запуску db1000n
 Echo.
+Echo 	введiть "3"  щоб виконати патч для старту сеансiв з командою встановлення та запуску mhddos_proxy
+Echo			     з використанням IP машин CloudShell (Тест!)
 Echo.
 Echo.
 Echo 	введiть "0" - для вiдновлення оригiнальних файлiв Google Cloud SDK (вiдмiна патчу)
@@ -22,6 +24,7 @@ Set /p choice=" Ваш вибiр: "
 if not defined choice goto m2
 if "%choice%"=="1" Copy /Y Bin\ssh.py "C:\Program Files (x86)\Google\Cloud SDK\google-cloud-sdk\lib\googlecloudsdk\command_lib\util\ssh" && Copy /Y Bin\1\Command(Operation-Stop_russia).txt C:\Windows\Temp && echo Patch GCloudSDK для mhddos_proxy встановлено && pause && exit
 if "%choice%"=="2" Copy /Y Bin\ssh.py "C:\Program Files (x86)\Google\Cloud SDK\google-cloud-sdk\lib\googlecloudsdk\command_lib\util\ssh" && Copy /Y Bin\2\Command(Operation-Stop_russia).txt C:\Windows\Temp && echo Patch GCloudSDK для db1000n встановлено && pause && exit
+if "%choice%"=="3" Copy /Y Bin\ssh.py "C:\Program Files (x86)\Google\Cloud SDK\google-cloud-sdk\lib\googlecloudsdk\command_lib\util\ssh" && Copy /Y Bin\3\Command(Operation-Stop_russia).txt C:\Windows\Temp && echo Test! Patch GCloudSDK для mhddos_proxy (--vpn) встановлено && pause && exit
 if "%choice%"=="0" Copy /Y Bin\0\ssh.py "C:\Program Files (x86)\Google\Cloud SDK\google-cloud-sdk\lib\googlecloudsdk\command_lib\util\ssh" && Del /q C:\Windows\Temp\Command(Operation-Stop_russia).txt && echo Вiдновлено оригiнальнi файли Google Cloud SDK && goto m2
 Echo.
 Echo  ! Некоректне введення
