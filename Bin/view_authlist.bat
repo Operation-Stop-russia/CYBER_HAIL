e6ae03b95b48109dd35aaa9af429a5c68fad617a
Operation-Stop-russia <103530329+Operation-Stop-russia@users.noreply.github.com>
2022-05-30 22:34:34 +0300

    Delete view_authlist.bat

--- Bin/view_authlist.bat
+++
@@ -1,8 +0,0 @@
-@Echo off
-
-(echo 1 && <null set/p "num=list") | gcloud init --skip-diagnostics
-echo No need
-echo.
-echo Виведено список авторизованих акаунтiв. Щоб закрити натиснiть будь-яку кнопку...
-pause
-exit
