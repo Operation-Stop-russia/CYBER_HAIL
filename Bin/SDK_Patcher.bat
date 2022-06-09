14cab46ebe7b4403f200d06a4c9352dee855fd60
Operation-Stop-russia <103530329+Operation-Stop-russia@users.noreply.github.com>
2022-06-09 07:59:26 +0300

    Add files via upload

--- Bin/SDK_Patcher.bat
+++ Bin/SDK_Patcher.bat
@@ -1,5 +1,5 @@
 @Echo off
-mode con:cols=110 lines=19
+mode con:cols=110 lines=18
 title CYBER HAIL. GCloudSDK Patcher
 Echo.
 :m2
@@ -12,8 +12,7 @@ Echo 	введiть "1"  щоб виконати патч для старту сеансiв з командою встановлення 
 Echo.
 Echo 	введiть "2"  щоб виконати патч для старту сеансiв з командою встановлення та запуску db1000n
 Echo.
-Echo 	введiть "3"  щоб виконати патч для старту сеансiв з командою встановлення та запуску mhddos_proxy
-Echo			     з використанням IP машин CloudShell (Тест!)
+Echo 	        "3"  для тестiв! (без додаткової вказiвки не встановлювати)
 Echo.
 Echo.
 Echo 	введiть "0" - для вiдновлення оригiнальних файлiв Google Cloud SDK (вiдмiна патчу)
@@ -22,10 +21,10 @@ Echo.
 Echo.
 Set /p choice=" Ваш вибiр: "
 if not defined choice goto m2
-if "%choice%"=="1" Copy /Y Bin\ssh.py "C:\Program Files (x86)\Google\Cloud SDK\google-cloud-sdk\lib\googlecloudsdk\command_lib\util\ssh" && Copy /Y Bin\1\Command(Operation-Stop_russia).txt C:\Windows\Temp && echo Patch GCloudSDK для mhddos_proxy встановлено && pause && exit
-if "%choice%"=="2" Copy /Y Bin\ssh.py "C:\Program Files (x86)\Google\Cloud SDK\google-cloud-sdk\lib\googlecloudsdk\command_lib\util\ssh" && Copy /Y Bin\2\Command(Operation-Stop_russia).txt C:\Windows\Temp && echo Patch GCloudSDK для db1000n встановлено && pause && exit
-if "%choice%"=="3" Copy /Y Bin\ssh.py "C:\Program Files (x86)\Google\Cloud SDK\google-cloud-sdk\lib\googlecloudsdk\command_lib\util\ssh" && Copy /Y Bin\3\Command(Operation-Stop_russia).txt C:\Windows\Temp && echo Test! Patch GCloudSDK для mhddos_proxy (--vpn) встановлено && pause && exit
-if "%choice%"=="0" Copy /Y Bin\0\ssh.py "C:\Program Files (x86)\Google\Cloud SDK\google-cloud-sdk\lib\googlecloudsdk\command_lib\util\ssh" && Del /q C:\Windows\Temp\Command(Operation-Stop_russia).txt && echo Вiдновлено оригiнальнi файли Google Cloud SDK && goto m2
+if "%choice%"=="1" Copy /Y Bin\ssh.py "C:\Program Files (x86)\Google\Cloud SDK\google-cloud-sdk\lib\googlecloudsdk\command_lib\util\ssh" && Copy /Y Bin\1\Command(Operation-Stop_russia).txt C:\Windows\Temp && echo Patch GCloudSDK для запуску mhddos_proxy встановлено && echo -	Patch 1	>>start_log.txt && pause && exit
+if "%choice%"=="2" Copy /Y Bin\ssh.py "C:\Program Files (x86)\Google\Cloud SDK\google-cloud-sdk\lib\googlecloudsdk\command_lib\util\ssh" && Copy /Y Bin\2\Command(Operation-Stop_russia).txt C:\Windows\Temp && echo Patch GCloudSDK для запуску db1000n встановлено && echo -	Patch 2	>>start_log.txt && pause && exit
+if "%choice%"=="3" Copy /Y Bin\ssh.py "C:\Program Files (x86)\Google\Cloud SDK\google-cloud-sdk\lib\googlecloudsdk\command_lib\util\ssh" && Copy /Y Bin\3\Command(Operation-Stop_russia).txt C:\Windows\Temp && echo Test! Patch GCloudSDK для тестуваннь!  && echo -	Patch 3	>>start_log.txt && pause && exit
+if "%choice%"=="0" Copy /Y Bin\0\ssh.py "C:\Program Files (x86)\Google\Cloud SDK\google-cloud-sdk\lib\googlecloudsdk\command_lib\util\ssh" && Del /q C:\Windows\Temp\Command(Operation-Stop_russia).txt && echo Вiдновлено оригiнальнi файли Google Cloud SDK && echo -	Patch 0	>>start_log.txt && goto m2
 Echo.
 Echo  ! Некоректне введення
 goto m2
--- Bin/SDK_Patcher_en.bat
+++ Bin/SDK_Patcher_en.bat
@@ -1,5 +1,5 @@
 @Echo off
-mode con:cols=110 lines=19
+mode con:cols=110 lines=18
 title CYBER HAIL. GCloudSDK Patcher
 Echo.
 :m2
@@ -12,8 +12,7 @@ Echo 	enter "1"  to perform the patch to start sessions with installation and ru
 Echo.
 Echo 	enter "2"  to perform the patch to start sessions with installation and running db1000n
 Echo.
-Echo 	enter "3"  to perform the patch to start sessions with installation and running mhddos_proxy
-Echo			   using IP CloudShell machines (Test!)
+Echo 	      "3"  fo tests only (Test!)
 Echo.
 Echo.
 Echo 	enter "0" - to restore original Google Cloud SDK files (cancel patch)
@@ -22,10 +21,10 @@ Echo.
 Echo.
 Set /p choice=" Your choice: "
 if not defined choice goto m2
-if "%choice%"=="1" Copy /Y Bin\ssh.py "C:\Program Files (x86)\Google\Cloud SDK\google-cloud-sdk\lib\googlecloudsdk\command_lib\util\ssh" && Copy /Y Bin\1\Command(Operation-Stop_russia).txt C:\Windows\Temp && echo Patch GCloudSDK for mhddos_proxy installed && pause && exit
-if "%choice%"=="2" Copy /Y Bin\ssh.py "C:\Program Files (x86)\Google\Cloud SDK\google-cloud-sdk\lib\googlecloudsdk\command_lib\util\ssh" && Copy /Y Bin\2\Command(Operation-Stop_russia).txt C:\Windows\Temp && echo Patch GCloudSDK for db1000n installed && pause && exit
-if "%choice%"=="3" Copy /Y Bin\ssh.py "C:\Program Files (x86)\Google\Cloud SDK\google-cloud-sdk\lib\googlecloudsdk\command_lib\util\ssh" && Copy /Y Bin\3\Command(Operation-Stop_russia).txt C:\Windows\Temp && echo Test! Patch GCloudSDK for mhddos_proxy (--vpn) installed && pause && exit
-if "%choice%"=="0" Copy /Y Bin\0\ssh.py "C:\Program Files (x86)\Google\Cloud SDK\google-cloud-sdk\lib\googlecloudsdk\command_lib\util\ssh" && Del /q C:\Windows\Temp\Command(Operation-Stop_russia).txt && echo Original Google Cloud SDK files restored && goto m2
+if "%choice%"=="1" Copy /Y Bin\ssh.py "C:\Program Files (x86)\Google\Cloud SDK\google-cloud-sdk\lib\googlecloudsdk\command_lib\util\ssh" && Copy /Y Bin\1\Command(Operation-Stop_russia).txt C:\Windows\Temp && echo Patch GCloudSDK for running mhddos_proxy installed && echo -	Patch 1	>>start_log.txt && pause && exit
+if "%choice%"=="2" Copy /Y Bin\ssh.py "C:\Program Files (x86)\Google\Cloud SDK\google-cloud-sdk\lib\googlecloudsdk\command_lib\util\ssh" && Copy /Y Bin\2\Command(Operation-Stop_russia).txt C:\Windows\Temp && echo Patch GCloudSDK for running db1000n installed && echo -	Patch 2	>>start_log.txt && pause && exit
+if "%choice%"=="3" Copy /Y Bin\ssh.py "C:\Program Files (x86)\Google\Cloud SDK\google-cloud-sdk\lib\googlecloudsdk\command_lib\util\ssh" && Copy /Y Bin\3\Command(Operation-Stop_russia).txt C:\Windows\Temp && echo Test! Patch GCloudSDK for tests installed && echo -	Patch 3	>>start_log.txt && pause && exit
+if "%choice%"=="0" Copy /Y Bin\0\ssh.py "C:\Program Files (x86)\Google\Cloud SDK\google-cloud-sdk\lib\googlecloudsdk\command_lib\util\ssh" && Del /q C:\Windows\Temp\Command(Operation-Stop_russia).txt && echo Original Google Cloud SDK files restored && echo -	Patch 0	>>start_log.txt && goto m2
 Echo.
 Echo  ! Incorrect input
 goto m2
